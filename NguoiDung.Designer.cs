499f86e03fa9758f70219419bc67b13742a1307f
hnhung31 <hnhunght31.10@gmail.com>
2023-12-05 10:00:17 +0700

    hong nhung

--- NguoiDung.Designer.cs
+++ NguoiDung.Designer.cs
@@ -181,7 +181,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(879, 166);
+            this.dataGridView1.Size = new System.Drawing.Size(866, 311);
             this.dataGridView1.TabIndex = 19;
             // 
             // bookCodeDataGridViewTextBoxColumn
